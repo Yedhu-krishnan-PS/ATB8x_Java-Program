ce38a594d1d2e1b955cd3d47af560c6f2e2149de
Yedhu-krishnan-PS <yedhuskrishnan@gmail.com>
2026-01-02 22:59:40 +0530

    Add the FOR LOOPS

--- src/Sept/ex_280924_Loops/Lab_032.java
+++ src/Sept/ex_280924_Loops/Lab_032.java
@@ -2,7 +2,8 @@ package Sept.ex_280924_Loops;
 
 public class Lab_032 {
     public static void main(String[] args) {
-        for (byte b = 97; b >=10; b++)
+        for (byte b = 73; b >=10; b++)
+
 
             System.out.println(b);
     }
