39d94c88737a10776ce7ccde8c1e84e376b9e768
Yedhu-krishnan-PS <yedhuskrishnan@gmail.com>
2026-01-03 23:10:55 +0530

    Add the FOR LOOPS

--- src/Sept/ex_200924/Lab_003.java
+++ src/Sept/ex_200924/Lab_003.java
@@ -4,5 +4,6 @@ public class Lab_003 {
     public static void main(String[] args) {
         System.out.println("Hi, This is Yedhu here!");
         System.out.println(5 * 4);
+        System.out.println(5 + 4);
     }
 }
