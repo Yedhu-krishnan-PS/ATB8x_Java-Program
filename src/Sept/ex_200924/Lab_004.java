9a35f3d20c12af6646d651abb1922341b37947a3
Yedhu-krishnan-PS <yedhuskrishnan@gmail.com>
2026-01-08 22:34:20 +0530

    Add the FOR LOOPS

--- src/Sept/ex_200924/Lab_004.java
+++ src/Sept/ex_200924/Lab_004.java
@@ -6,5 +6,6 @@ public class Lab_004 {
      public static void main(String[] args) {
           System.out.println("Hello World!");
           System.out.println(5 + 4);
+          System.out.println(5 - 4);
      }
 }
